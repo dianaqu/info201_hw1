6e1f98d41aed8279b7f6de0c013d8590308371b5
dianaqu <xiao19921201@gmail.com>
2017-04-02 11:20:13 -0700

    sh file added

---
+++ session.sh
@@ -0,0 +1,25 @@
+Dianas-MacBook-Air:info 201 xiaoyuqu$ git clone https://github.com/dianaqu/info201_hw1.git
+Cloning into 'info201_hw1'...
+remote: Counting objects: 3, done.
+remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (3/3), done.
+Dianas-MacBook-Air:info 201 xiaoyuqu$ cd info201_hw1
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ ls
+README.md
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ open README.md
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ git init
+Reinitialized existing Git repository in /Users/xiaoyuqu/Dropbox/INFO 201/info201_hw1/.git/
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ git add .
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ git commit -m "overall strucute made"
+[master eaea6ab] overall strucute made
+ 1 file changed, 17 insertions(+), 1 deletion(-)
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ git push
+Counting objects: 3, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 340 bytes | 0 bytes/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/dianaqu/info201_hw1.git
+   e31e19d..eaea6ab  master -> master
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ touch session.sh
+Dianas-MacBook-Air:info201_hw1 xiaoyuqu$ open session.sh
